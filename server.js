93de7ea974bc6b99f0d540f9117d401b86c82134
Lucas Martins <dev.lucasmartins@gmail.com>
2019-11-13 15:20:24 -0300

    added config.js

--- server.js
+++ server.js
@@ -8,6 +8,38 @@ const compression = require('compression');
 
 const zlib = require('zlib');
 
+
+
+const fs = require('fs');
+// This is good for local dev environments, when it's better to
+// store a projects environment variables in a .gitignore'd file
+require('dotenv').config();
+
+function getEnvironmentVariable(key, _default = '') { return process.env[key] || _default; }
+
+function createEnvironementFile() {
+  return `export const environment = {
+  production: true,
+  oauthBaseUrl: '${getEnvironmentVariable('OAUTH2_BASE_URL')}',
+  oauthClientId: '${getEnvironmentVariable('OAUTH2_CLIENT_ID')}'
+};`;
+}
+
+const environment = getEnvironmentVariable('ENVIRONMENT');
+const environmentFile = createEnvironementFile();
+
+console.log(`
+  ENVIRONMENT -> ${environment}
+  ---
+  ${environmentFile}
+`);
+
+fs.writeFile(`./src/environments/environment.ts`, environmentFile, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
 app.use(express.static(__dirname + '/dist/ng-accounts'));
 
 app.disable('etag');
