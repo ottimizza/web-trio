1b1d8beb7e3a4a1627f3b32ff1fdd5ded2f15d53
Lucas Martins <dev.lucasmartins@gmail.com>
2019-11-18 14:47:29 -0300

    added .env file again

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   oauthBaseUrl: 'https://development-oauth-server.herokuapp.com',
-  oauthClientId: 'bussola-contabil-client'
+  oauthClientId: '76179baad962d1b8ce4d'
 };
