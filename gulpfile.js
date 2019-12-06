5f07fa847ed56a3b765b0119113b82236a94fd57
Lucas Martins <dev.lucasmartins@gmail.com>
2019-12-06 17:53:35 -0300

    adjusment

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-var gulp = require('gulp');
-var gzip = require('gulp-gzip');
+// var gulp = require('gulp');
+// var gzip = require('gulp-gzip');
 
-gulp.task('compress', function() {
-  return gulp.src(['./dist/ng-accounts/**/*.*'])
-             .pipe(gzip())
-             .pipe(gulp.dest('./dist'))
-});
+// gulp.task('compress', function() {
+//   return gulp.src(['./dist/**/*.*'])
+//              .pipe(gzip())
+//              .pipe(gulp.dest('./dist'))
+// });
