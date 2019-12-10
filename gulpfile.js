8b6eed720a37f066e922d6ecd1dd470d506bfdb7
Lucas Martins <dev.lucasmartins@gmail.com>
2019-12-10 09:26:51 -0300

    removed gulp compress

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-// var gulp = require('gulp');
-// var gzip = require('gulp-gzip');
+var gulp = require('gulp');
+var gzip = require('gulp-gzip');
 
-// gulp.task('compress', function() {
-//   return gulp.src(['./dist/ng-accounts/*.*'])
-//              .pipe(gzip())
-//              .pipe(gulp.dest('./dist/ng-accounts'))
-// });
+gulp.task('compress', function() {
+  return gulp.src(['./dist/ng-accounts/*.*'])
+             .pipe(gzip())
+             .pipe(gulp.dest('./dist/ng-accounts'))
+});
