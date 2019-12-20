718e0730fc1d0ebc59618b464bb520d3be45c4d6
Lucas Martins <dev.lucasmartins@gmail.com>
2019-12-20 11:14:31 -0300

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  oauthBaseUrl: 'https://development-oauth-server.herokuapp.com',
-  oauthClientId: '58a94562a7a7502d4c1b',
-  storageBaseUrl: 'https://s4.ottimizzacontabil.com:55325',
-  storageApplicationId: 'account-avatar',
+  oauthBaseUrl: '',
+  oauthClientId: '',
+  storageBaseUrl: '',
+  storageApplicationId: 'accounts-avatar',
   storageAccountingId: 'ottimizza'
 };
