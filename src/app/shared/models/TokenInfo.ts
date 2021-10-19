2080616ef553245527b887ce06fb94e9490e7e86
Rodrigo <rodrigo.h.m.112200@gmail.com>
2021-10-19 14:35:49 -0300

    .

--- src/app/shared/models/TokenInfo.ts
+++ src/app/shared/models/TokenInfo.ts
@@ -1,36 +1,67 @@
 import { TypeConversorUtils } from '@shared/utils/type-conversor.utils';
-import { User } from '@shared/models/User';
+import { Organization } from './Organization';
+import { TokenInfo, Authority } from './TokenInfo';
 
-export enum Authority {
-  ADMIN = 'ADMIN',
-  READ = 'READ',
-  WRITE = 'WRITE',
+export class UserAdditionalInformation {
+  public role: string;
+  public birthDate: string;
+  public accountingDepartment: string;
 }
 
-export class TokenInfo {
+export class User {
 
-  authenticated: boolean;
+  static Type = class {
+    static ADMINISTRATOR = 0;
+    static ACCOUNTANT = 1;
+    static CUSTOMER = 2;
+  };
+
+  id: number;
+  username: string;
+  password: string;
+
+  active: boolean;
+  activated: boolean;
+  type: number;
+
+  avatar: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+
+  organization: Organization;
+
+  additionalInformation: UserAdditionalInformation;
+
   authorities: { authority: Authority }[];
-  clientOnly: false;
-  credentials: string;
-  details: any;
-  name: string;
-  oauth2Request: any;
-  principal: any;
-  userAuthentication: any;
 
-  public static fromLocalStorage(): TokenInfo {
-    const storedToken = JSON.parse(localStorage.getItem('token-info') || '{}');
-    return TypeConversorUtils.fromAny<TokenInfo>(storedToken, new TokenInfo());
+  static fromLocalStorage(): User {
+    const storedUser = JSON.parse(localStorage.getItem('user-info') || '{}');
+    return TypeConversorUtils.fromAny<User>(storedUser, new User());
   }
+
+  public static allInfoFromLocalStorage(): User & TokenInfo {
+    return Object.assign(this.fromLocalStorage(), TokenInfo.fromLocalStorage());
+  }
+
+  public get fullName() {
+    return `${this.firstName || this.lastName || ''}`.trim();
+  }
+
+  isCustomer = () => this.type === User.Type.CUSTOMER;
+
+  isAccountant = () => this.type === User.Type.ACCOUNTANT;
+
+  isAdministrator = () => this.type === User.Type.ADMINISTRATOR;
 
   canManage = () => this._can(Authority.ADMIN);
   canEdit = () => this._can(Authority.WRITE);
   canView = () => this._can(Authority.READ);
 
   private _can(aut: Authority) {
-    const userInfo: TokenInfo = User.allInfoFromLocalStorage() as any;
-    return userInfo.authorities.map(au => au.authority).includes(aut);
+    // const userInfo: TokenInfo = User.fromLocalStorage() as any;
+    return this.authorities.map(au => au.authority).includes(aut);
   }
 
 }
