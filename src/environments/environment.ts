b6f04d35b397077df46e1f326c6576ff5069b52e
Rodrigo <rodrigo.h.m.112200@gmail.com>
2022-05-05 10:21:53 -0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   oauthBaseUrl: 'https://dev-tareffa-oauth-server.herokuapp.com',
   oauthClientId: '58a94562a7a7502d4c1b',
   serviceUrl: 'https://api-parceiros.herokuapp.com',
-  trioClientId: '9f17de5f-7040-45e8-aed3-0315875d7c2e',
+  trioClientId: '',
   allowedInstitutions: ['itau_br_business', 'bradesco_br_business', 'santander_br_business', 'caixa_br_business']
 };
